75f2e6b981499c37a4ae2452b90734b8850e2da5
Anh Tú <at.mrsteven@gmail.com>
2023-07-28 16:54:30 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/login.blade.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LoginView</title>
+</head>
+<body>
+    <h1>Welcome to My Laravel App!</h1>
+    <p>This is a Blade view file.</p>
+</body>
+</html>
